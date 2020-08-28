50282684a2a5b181f9b4a6153d530240cd6fb1f2
kshiflett88 <kodishiflett@gmail.com>
2020-08-28 12:50:19 -0700

    seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,26 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-Post.destroy_all
+# Post.destroy_all
 
 u1 = User.create( username: 'Kreator', email: 'kodikreator', password: '123456', authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "", cover_photo: "" )
-u2 = User.create( username: "Rengoku", email: "fire", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
-u3 = User.create( username: "San", email: "Moro", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
-u4 = User.create( username: "Chihiro", email: "dragon", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
-u5 = User.create( username: "Natsu", email: "igneel", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
-u6 = User.create( username: "Kocho", email: "butterfly", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
-u7 = User.create( username: "Legosi", email: "bunny", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
-u8 = User.create( username: "Zenitsu", email: "lightening", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
+# u2 = User.create( username: "Rengoku", email: "fire", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
+# u3 = User.create( username: "San", email: "Moro", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
+# u4 = User.create( username: "Chihiro", email: "dragon", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
+# u5 = User.create( username: "Natsu", email: "igneel", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
+# u6 = User.create( username: "Kocho", email: "butterfly", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
+# u7 = User.create( username: "Legosi", email: "bunny", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
+# u8 = User.create( username: "Zenitsu", email: "lightening", password: "123456", authored_post_ids: [], wall_post_ids: [], requestees: [], requesters: [], profile_photo: "" , cover_photo: "" )
 
-p1 = Post.create( body: "ASHITAAAAAKKAAA!", author_id: u3.id, wall_id: u1.id )
-p2 = Post.create( body: "Train Harder!" , author_id: u2.id, wall_id: u8.id )
-p3 = Post.create( body: "This bath house aint so bad...", author_id: u4.id, wall_id: u4.id )
-p4 = Post.create( body: "I'm fired up!", author_id: u5.id, wall_id: u2.id )
-p5 = Post.create( body: ".....?", author_id: u6.id, wall_id: u2.id )
-p6 = Post.create( body: "I Love you Haru Chan!", author_id: u7.id, wall_id: u7.id )
-p7 = Post.create( body: "Nezuko!!!", author_id: u8.id, wall_id: u8.id )
-p8 = Post.create( body: "狼が大好き", author_id: u４.id, wall_id: u３.id )
-p9 = Post.create( body: "Welcome to my app!", author_id: u1.id, wall_id: u1.id )
+# p1 = Post.create( body: "ASHITAAAAAKKAAA!", author_id: u3.id, wall_id: u1.id )
+# p2 = Post.create( body: "Train Harder!" , author_id: u2.id, wall_id: u8.id )
+# p3 = Post.create( body: "This bath house aint so bad...", author_id: u4.id, wall_id: u4.id )
+# p4 = Post.create( body: "I'm fired up!", author_id: u5.id, wall_id: u2.id )
+# p5 = Post.create( body: ".....?", author_id: u6.id, wall_id: u2.id )
+# p6 = Post.create( body: "I Love you Haru Chan!", author_id: u7.id, wall_id: u7.id )
+# p7 = Post.create( body: "Nezuko!!!", author_id: u8.id, wall_id: u8.id )
+# p8 = Post.create( body: "狼が大好き", author_id: u４.id, wall_id: u３.id )
+# p9 = Post.create( body: "Welcome to my app!", author_id: u1.id, wall_id: u1.id )
 
 # u1.authored_post_ids= [p9.id]
 # u1.wall_post_ids= [p1.id, p9.id]
